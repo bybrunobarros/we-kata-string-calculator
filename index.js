9f186caa190fb8ec4b71527633a514e62e49b169
bruno <bybrunobarros@gmail.com>
2020-11-26 14:29:08 +0100

    add three test cases
    This is an awful commit message

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const add = (numbers) => {
   if (numbers === "") return 0;
-}
+
+  return numbers
+    .split(/[\n,]/)
+    .reduce((result, num) => result + parseInt(num, 10), 0);
+};
--- index.test.js
+++ index.test.js
@@ -1,8 +1,27 @@
-import test from "ava"
-import {add} from "./index.js"
+import test from "ava";
+import { add } from "./index.js";
 
-test("should return an empty when input is 0", t => {
+test("should return 0 when numbers is an empty string", (t) => {
   const result = add("");
 
   t.is(0, result);
-})
+});
+
+test("should return a number when numbers contains only one number", (t) => {
+  const result = add("1");
+
+  t.is(1, result);
+});
+
+test("should return the sum of two numbers when numbers contains two numbers", (t) => {
+  const result = add("1,2");
+
+  t.is(3, result);
+});
+
+test("should return sum when numbers are separated by new line character", (t) => {
+  const result = add(`1
+2,3`);
+
+  t.is(6, result);
+});
